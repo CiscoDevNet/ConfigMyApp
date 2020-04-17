53ff1276c1b81058016b0181877138dab94e6ee9
Israel Ogbole <iogbole@gmail.com>
2020-04-17 20:21:30 +0100

    Revert "Merge pull request #6 from Appdynamics/feature/http_proxy"
    This reverts commit d2bd0cb448bae4b3f186ecd798d4a0fbd3b892c7, reversing
    changes made to 694f07e4d327e92e19ba93538d785422f2972d5c.

--- configMyApp.sh
+++ configMyApp.sh
@@ -17,10 +17,9 @@
     exit 1
 }
 
-conf_file="config.json"
+conf_file="testconfig.json"
 
 overwrite_health_rules=$(jq -r '.overwrite_health_rules' <${conf_file})
-are_passwords_encoded=$(jq -r '.are_passwords_encoded' <${conf_file})
 
 prod_controller=$(jq -r ' .prod_controller_details[].url' <${conf_file})
 prod_username=$(jq -r ' .prod_controller_details[].username' <${conf_file})
@@ -31,10 +30,6 @@ dev_controller=$(jq -r ' .non_prod_controller_details[].url' <${conf_file})
 dev_username=$(jq -r ' .non_prod_controller_details[].username' <${conf_file})
 dev_password=$(jq -r ' .non_prod_controller_details[].password' <${conf_file})
 dev_serverVizAppID=$(jq -r ' .non_prod_controller_details[].server_viz_app_id' <${conf_file})
-
-dev_proxy_url=$(jq -r ' .non_prod_controller_details[].proxy_ur'l <${conf_file})
-dev_proxy_port=$(jq -r ' .non_prod_controller_details[].proxy_port' <${conf_file})
-
 #echo "Dev $dev_controller >  $dev_username >  $dev_password > $dev_serverVizAppID"
 
 # Do not change anything else beyond this point except you know what you're doing :)
@@ -43,9 +38,6 @@ dev_proxy_port=$(jq -r ' .non_prod_controller_details[].proxy_port' <${conf_file
 vanilla_noDB="./dashboards/CustomDashboard_noDB_vanilla.json"
 vanilla="./dashboards/CustomDashboard_vanilla.json"
 
-vanilla_noSIM="./dashboards/CustomDashboard_noSIM_vanilla.json"
-vanilla_noDB_noSIM="./dashboards/CustomDashboard_noDB_noSIM_vanilla.json"
-
 #init HR templates
 serverVizHealthRuleFile="./healthrules/ServerHealthRules.xml"
 applicationHealthRule="./healthrules/ApplicationHealthRules.xml"
@@ -53,8 +45,6 @@ applicationHealthRule="./healthrules/ApplicationHealthRules.xml"
 #init template placeholder
 templateAppName="ChangeApplicationName"
 templateDBName="ChangeDBName"
-
-tempFolder="temp"
 
 bt_folder="./business_transactions"
 
@@ -116,17 +106,8 @@ fi
 
 echo "You entered '$DBName' for Database collector name"
 echo ""
-
 if [ "$3" != "" ]; then
-    includeSIM=$3
-else
-    echo ""
-    echo "Include Server Visibility?"
-    read -p "Enter 'no' if you do not want to include SIM [ENTER]:  " includeSIM
-fi
-
-if [ "$4" != "" ]; then
-    controller=$4
+    controller=$3
 else
     echo ""
     echo "What's your application environment?"
@@ -136,32 +117,13 @@ fi
 echo "You entered '$controller' for application evironment"
 echo ""
 
-if [ "$5" != "" ]; then
-    configbt=$5
+if [ "$4" != "" ]; then
+    configbt=$4
 else
     configbt="no"
 fi
 
 echo "You entered '$configbt' for transaction configuration"
-echo ""
-# end input params
-
-# validate input params
-if [ "$includeSIM" = "YES" ] || [ "$includeSIM" = "yes" ] || [ "$includeSIM" = "Yes" ] || [ "$includeSIM" = "y" ] || [ "$includeSIM" = "Y" ] || [ "$includeSIM" = "sim" ] || [ "$includeSIM" = "SIM" ] || [ "$includeSIM" = "Sim" ]; then
-    includeSIM="true"
-elif [ "$includeSIM" = "NO" ] || [ "$includeSIM" = "no" ] || [ "$includeSIM" = "No" ] || [ "$includeSIM" = "n" ] || [ "$includeSIM" = "N" ] || [ "$includeSIM" = "nosim" ] || [ "$includeSIM" = "NOSIM" ] || [ "$includeSIM" = "Nosim" ]; then
-    includeSIM="false"
-else
-    echo "You must enter valid yes/no value, set includeSIM to no if you're not interested in Server Visibility"
-    exit 1
-fi
-
-if [ "$includeSIM" = "true" ] && [ "$prod_serverVizAppID" = "" ] && [ "$dev_serverVizAppID" = "" ]; then
-    echo "Server visibility application ID must be defined when SIM is enabled. Set includeSIM value to no if you are not interested in SIM moitoring."
-    exit 1
-fi
-
-echo "Server Visibility is set to '$includeSIM'"
 echo ""
 
 if [ "$appName" = "" ] || [ "$DBName" = "" ]; then
@@ -174,106 +136,29 @@ if [ "$controller" = "prod" ] || [ "$controller" = "production" ] || [ "$control
     password=${prod_password}
     username=${prod_username}
     serverVizAppID=${prod_serverVizAppID}
-    proxy_url="${prod_proxy_url}"
-    proxy_port="${prod_proxy_port}"
 else
     hostname=${dev_controller}
     password=${dev_password}
     username=${dev_username}
     serverVizAppID=${dev_serverVizAppID}
-    proxy_url="${dev_proxy_url}"
-    proxy_port="${dev_proxy_port}"
-fi
 
-# decode passwords if encoded
-if [ "$are_passwords_encoded" = "true" ]; then
-    password=$(eval echo ${password} | base64 --decode) 
 fi
 
 echo "Using $hostname controller"
 
-# functions >>>
-function func_check_http_status {
-    local http_code=$1
-    local message_on_failure=$2
-    echo "HTTP status code: $http_code"
-    if [[ $http_code -lt 200 ]] || [[ $http_code -gt 299 ]]; then
-        echo $message_on_failure
-        func_cleanup
-        exit 1 
-    fi
-}
-
-function func_copy_file_and_replace_values {
-    local filePath=$1
-    #local appName=$2 # todo
-    #local DBName=$3
-
-    # make a temp file
-    fileName="$(basename -- $filePath)"
-    mkdir -p "$tempFolder" && cp -r $filePath ./$tempFolder/$fileName
-
-    # replace values
-    sed -i.original -e "s/${templateAppName}/${appName}/g; s/${templateDBName}/${DBName}/g" "${tempFolder}/${fileName}"
-    
-    # return full file path
-    echo "${tempFolder}/${fileName}"
-}
-
-function func_cleanup {
-    # remove all from temp folder
-    rm -rf $tempFolder
-}
-
-#Process proxy details
-#jq sets empty strings to null istead of NULL
-if [ -z "$proxy_url" ] || [ "$proxy_url" = "null" ]  || [ -z "$proxy_port" ] || [ "$proxy_port" = "null" ] || [ "$proxy_port" = "" ] || [ "$proxy_url" = "" ]; then 
-    echo "No HTTP Proxy is configured. Skipping proxy configuration..." 
-    proxy_details=""
-else 
-    echo "Found HTTP Proxy configuration, using... " 
-    echo "Proxy URL = $proxy_url , Proxy Port = $proxy_port"
-    proxy_details="-x $proxy_url:$proxy_port"
-fi
-
-
-# start importing >>>
 endpoint="/controller/CustomDashboardImportExportServlet"
 url=${hostname}${endpoint}
 
 echo "Import Dash URL $url"
 echo ""
 echo ""
-
-# check if app exists
-echo "Check if app exists: curl --user ${username}:${password} ${hostname}/controller/rest/applications?output=JSON ${proxy_details}"
-allApplications=$(curl --user ${username}:${password} ${hostname}/controller/rest/applications?output=JSON ${proxy_details}) 
-
-applicationObject=$(jq --arg appName "$appName" '.[] | select(.name == $appName)' <<< $allApplications)
-
-if [ "$applicationObject" = "" ]; then
-    func_check_http_status 404 "Application '"$appName"' not found."
-fi
+echo "Creating Server Viz Health Rules..."
+sleep 3
 
 #ServerViz health rules
-if [ "$includeSIM" = "true" ]; then
-
-    # check if server visibility application id exists
-    httpCode=$(curl -I -s -o /dev/null -w "%{http_code}" --user ${username}:${password} ${hostname}/controller/rest/applications/${serverVizAppID} ${proxy_details})
-
-    func_check_http_status $httpCode "Server visibility application id '"$serverVizAppID"' not found."
-
-    echo "Creating Server Viz Health Rules..."
-
-    #sed -i.bak -e "s/${templateAppName}/${appName}/g" ${serverVizHealthRuleFile}
-    pathToHealthRulesFile=$(func_copy_file_and_replace_values ${serverVizHealthRuleFile})
-    
-    httpCode=$(curl -X POST -o /dev/null -w "%{http_code}" --user ${username}:${password} ${hostname}/controller/healthrules/${serverVizAppID}?overwrite=${overwrite_health_rules} -F file=@${pathToHealthRulesFile} ${proxy_details})
-
-    func_check_http_status $httpCode "Saving server visibility health rules for application id '"$serverVizAppID"' failed."
-
-fi
-
+sed -i.bak -e "s/${templateAppName}/${appName}/g" ${serverVizHealthRuleFile}
+curl -X POST --user ${username}:${password} ${hostname}/controller/healthrules/${serverVizAppID}?overwrite=${overwrite_health_rules} -F file=@${serverVizHealthRuleFile}
+sleep 4
 #Application health rules
 echo "Creating ${appName} Health Rules..."
 sleep 4
@@ -281,89 +166,59 @@ sleep 4
 echo "URL ecoding App Name"
 sleep 1
 encodeAppName=$(IOURLEncoder $appName)
-echo "Encoded AppName is: $encodeAppName"
+echo "Encoded AppName is :  $encodeAppName"
 echo ""
-httpCode=$(curl -X POST -o /dev/null -w "%{http_code}" --user ${username}:${password} ${hostname}/controller/healthrules/$encodeAppName?overwrite=${overwrite_health_rules} -F file=@${applicationHealthRule} ${proxy_details})
-
-func_check_http_status $httpCode "Saving application health rules for application id '"$serverVizAppID"' failed."
-
+echo ""
+curl -X POST --user ${username}:${password} ${hostname}/controller/healthrules/$encodeAppName?overwrite=${overwrite_health_rules} -F file=@${applicationHealthRule}
 sleep 1
 echo ""
-
+echo ""
 echo "Processing Dashboard Template."
 sleep 3
 echo ""
-
+echo ""
 #Dashboard
-echo "Applying Database and SIM settings..."
+echo "Applying Database settings..."
 sleep 1
-if [ "$DBName" = "NO" ] || [ "$DBName" = "no" ] || [ "$DBName" = "none" ] || [ "$DBName" = "nodb" ] || [ "$DBName" = "NODB" ]; then
-
-    if [ "$includeSIM" = "true" ]; then
-        templateFile="$vanilla_noDB"
-    else
-        templateFile="$vanilla_noDB_noSIM"
-    fi
+if [ "$DBName" = "NO" ] || [ "$DBName" = "no" ] || [ "$DBName" = "none" ]; then
+    templateFile="$vanilla_noDB"
     #sed -i.DBbak -e '885,948d;1201,1242d' ${templateFile}
 else
-    if [ "$includeSIM" = "true" ]; then
-        templateFile="$vanilla"
-    else
-        templateFile="$vanilla_noSIM"
-    fi
+    templateFile="$vanilla"
 fi
 
-echo "Template file is: $templateFile"
-
 dt=$(date '+%Y-%m-%d_%H-%M-%S')
-#take a backup as .bak, then find and replace
-# sed -i.bak -e "s/${templateAppName}/${appName}/g; s/${templateDBName}/${DBName}/g" ${templateFile}
-pathToDashboardFile=$(func_copy_file_and_replace_values ${templateFile})
+#take a backup  as .bak, then find and replace
+sed -i.bak -e "s/${templateAppName}/${appName}/g; s/${templateDBName}/${DBName}/g" ${templateFile}
 
-echo "Create dashboard"
-sleep 3
+response=$(curl -X POST --user ${username}:${password} ${url} -F file=@${templateFile})
 
-httpCode=$(curl -X POST -o /dev/null -w "%{http_code}" --user ${username}:${password} "${url}" -F file=@${pathToDashboardFile} ${proxy_details})
-
-func_check_http_status $httpCode "Error occured while creating dashboard."
-
-#if [[ "$response" = *"$appName"* ]]; then
-#    echo "*********************************************************************"
-#    echo "The dashboard was created successfully. "
-#    echo "Please check the $hostname controller "
-#    echo "The Dashboard name is '$appName:App Visibility Pane' "
-#    echo "*********************************************************************"
-#else
-#    echo "Error occured in creating dashboard. See details below:"
-#    echo "$response"
-#fi
+if [[ "$response" = *"$appName"* ]]; then
+    echo "*********************************************************************"
+    echo "The dashboard was created successfully. "
+    echo "Please check the $hostname controller "
+    echo "The Dashboard name is '$appName:App Visibility Pane' "
+    echo "*********************************************************************"
+else
+    echo "Error occured in creating dashboard. See details below:"
+    echo "$response"
+fi
 
 echo ""
 echo ""
 sleep 3
 echo "Restoring vanilla template files... please wait.."
-#sleep 5
-
+sleep 5
 #restore original template files for next use
-#mv "${serverVizHealthRuleFile}".bak "${serverVizHealthRuleFile}"
-cp -rf "./${tempFolder}" "./dashboards/uploaded/${appName}"."${dt}"
+mv "${serverVizHealthRuleFile}".bak "${serverVizHealthRuleFile}"
+cp "${templateFile}" "./dashboards/uploaded/${appName}"."${dt}".json
 
-#if [ "$DBName" = "NO" ] || [ "$DBName" = "no" ] || [ "$DBName" = "none" ] || [ "$DBName" = "None" ] || [ "$DBName" = "No" ]; then
-#    if [ "$includeSIM" = "true" ]; then
-#        #cp $vanilla_noDB $templateFile
-#        mv "${templateFile}".bak "${vanilla_noDB}"
-#    else
-#        mv "${templateFile}".bak "${vanilla_noDB_noSIM}"
-#    fi
-#else
-#    if [ "$includeSIM" = "true"]; then
-#        mv "${templateFile}".bak "${vanilla}"
-#    else
-#        mv "${templateFile}".bak "${vanilla_noSIM}"
-#    fi
-#fi
-
-func_cleanup
+if [ "$DBName" = "NO" ] || [ "$DBName" = "no" ] || [ "$DBName" = "none" ] || [ "$DBName" = "None" ] || [ "$DBName" = "No" ]; then
+    #cp $vanilla_noDB $templateFile
+    mv "${templateFile}".bak "${vanilla_noDB}"
+else
+    mv "${templateFile}".bak "${vanilla}"
+fi
 
 echo ""
 echo ""
@@ -376,7 +231,7 @@ if [ "$configbt" = "configbt" ] || [ "$configbt" = "yes" ] || [ "$configbt" = "b
     if [ -f "${FILE}" ]; then
         echo "${FILE} exist"
         btendpoint="/controller/transactiondetection/${appName}/custom"
-        response=$(curl -X POST --user ${username}:${password} ${hostname}${btendpoint} -F file=@${FILE} ${proxy_details})
+        response=$(curl -X POST --user ${username}:${password} ${hostname}${btendpoint} -F file=@${FILE})
         if [[ "$response" = *"HTTP/1.1 200 OK"* ]]; then
             echo "Created Business transaction rules successfully"
         else
