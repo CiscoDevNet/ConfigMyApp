04e79df34f232a403555550152727cc61e9f42f6
Israel Ogbole <iogbole@yahoo.com>
2020-08-15 22:18:50 +0100

    Create upload_dasboards.sh

---
+++ dashboards/upload_dasboards.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# 1. INPUT PARAMETERS
+_controller_url=${1}   # hostname + /controller
+_user_credentials=${2} # ${username}:${password}
+
+_application_name=${3}
+
+_custom_dash_dir="./custom_dashboards" #I might move this into dashboard folder.. let see..
+_temp_dash_dir="$_custom_dash_dir/temp"
+templateAppName="ChangeApplicationName"
+
+# 2. FUNCTIONS
+function func_check_http_response() {
+    local http_message_body="$1"
+    local string_success_response_contains="$2"
+    if [[ "$http_message_body" =~ "$string_success_response_contains" ]]; then # contains
+        echo "Success..."
+    else
+        echo "${dt} ERROR "{$http_message_body}"" >>error.log
+        echo "ERROR $http_message_body"
+        #exit 1
+    fi
+}
+
+# application id
+allApplications=$(curl -s --user ${_user_credentials} ${_controller_url}/rest/applications?output=JSON)
+
+applicationObject=$(jq --arg appName "$_application_name" '.[] | select(.name == $appName)' <<<$allApplications)
+
+if [ "$applicationObject" = "" ]; then
+    func_check_http_response 404 "Application '"$_application_name"' not found. Aborting..."
+fi
+
+_application_id=$(jq '.id' <<<$applicationObject)
+
+_dasboard_API_endpoint="/CustomDashboardImportExportServlet"
+
+_vanilla_files="./custom_dashboards/*.json"
+
+mkdir -p "$_temp_dash_dir" && cp -r "$_custom_dash_dir/*.json" "$_temp_dash_dir"
+
+sed -i -e "s/${templateAppName}/${_application_name}/g" "$_temp_dash_dir/*.json"
+
+for dashFile in $_temp_dash_dir; do
+    echo "Processing $dashFile dashboard template"
+
+    response=$(curl -s -X POST --user ${_user_credentials} ${_controller_url}/${_dasboard_API_endpoint} -F file=@${dashFile})
+
+    expected_response='"success":true'
+
+    func_check_http_response "\{$response}" $expected_response
+
+    echo "*********************************************************************"
+    echo "The $dashFile dashboard was created successfully. "
+    echo "Please check the $_controller_url controller "
+    echo "*********************************************************************"
+
+    echo ""
+    sleep 1
+
+    # save used uploaded files
+    mkdir -p "${_custom_dash_dir}/uploaded"
+
+    cp -rf "./${_temp_dash_dir}/${dashFile}" "${_custom_dash_dir}/uploaded/${_application_name}"-"${dashFile}"
+
+    #clean up - one at a time
+    rm -rf "./${_temp_dash_dir}/${dashFile}"
+
+done
