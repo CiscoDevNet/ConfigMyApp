e421441d70e83d59cf2f75810a6246a6f9cd022b
alex_jov <aleksjovovic@gmail.com>
2020-08-19 20:14:51 +0100

    Action suppression bugfix missing functions and folders; Save uploaded actions;

--- api_actions/upload-files-action-suppression.sh
+++ api_actions/upload-files-action-suppression.sh
@@ -19,6 +19,16 @@ function func_check_http_response(){
     fi
 }
 
+function func_check_http_status() {
+    local http_code=$1
+    local message_on_failure=$2
+    if [[ $http_code -lt 200 ]] || [[ $http_code -gt 299 ]]; then
+        echo "${dt} ERROR "{$http_code: $message_on_failure}"" >> error.log
+        echo "ERROR $http_code: $message_on_failure"
+        exit 1
+    fi
+}
+
 # 3. PREPARE REQUEST
 dt=$(date '+%Y-%m-%d_%H-%M-%S')
 
@@ -36,11 +46,17 @@ fi
 _application_id=$(jq '.id' <<< $applicationObject)
 _resource_url="alerting/rest/v1/applications/${_application_id}/action-suppressions"
 
+# make a directory if not exist
+mkdir -p ./api_actions/actions
+mkdir -p ./api_actions/uploaded
 _action_suppression_files="./api_actions/actions/*.json"
 
-
 for f in $_action_suppression_files; do
-    echo "Processing $f action suppression file"
+    
+    # Check if folder contains files
+    [ -f "$f" ] || func_check_http_status 404 "No files found that match pattern: '"$_action_suppression_files"'. Aborting..."
+
+    echo "Processing '"$f"' action suppression file"
 
     _payload_path=$f
 
@@ -50,6 +66,9 @@ for f in $_action_suppression_files; do
     # 5. CHECK RESULT
     expected_response='"id":' # returns id on success
     func_check_http_response "\{$response}" $expected_response
+
+    fileName="$(basename -- $f)"
+    cp -rf "$_payload_path" "./api_actions/uploaded/${fileName}.${dt}"
 
     # echo "response is: $response"
 
