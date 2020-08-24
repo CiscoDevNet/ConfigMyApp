f37a79140b331ea013c4d7685722cf25352c3399
alex_jov <aleksjovovic@gmail.com>
2020-08-24 18:05:22 +0100

    Merging action suppression from Develop (#34)
    * refactor app health rules (#27)
    
    * Feature/containerHealthRules (#28)
    
    * Feature/disable node hr (#29)
    
    * microservice health rules
    
    * Merge action-suppression and port-bugfix to develop from feature b;
    
    * Add action suppression upload from file; Typos;
    
    * Add suppress action config;
    
    * Add actions folder and example file;
    
    * Dash layout
    
    * Adding detele action suppression;
    
    * Controller port as parameter takes precedence;
    
    * Port controller;
    
    * Update create and delete action suppression; Update config;
    
    * Action suppression bugfix missing functions and folders; Save uploaded actions;
    
    * Action suppression adding proxy; Fix empty proxy; Backup only uploaded files;
    
    * Update action suppression help;
    
    * Update QAConfigMyApp.yml
    
    * Update QAConfigMyApp.yml
    
    Co-authored-by: Israel Ogbole <iogbole@yahoo.com>

--- api_actions/application-action-suppression.sh
+++ api_actions/application-action-suppression.sh
@@ -4,19 +4,24 @@
 _controller_url=${1} # hostname + /controller
 _user_credentials=${2} # ${username}:${password}
 
-_action_suppression_start=${3}
-_action_suppression_duration=${4} 
+actions_proxy_details=${3}
 
-_application_name=${5}
+_action_suppression_start=${4}
+_action_suppression_duration=${5} 
 
-_action_suppression_name=${6}
+_application_name=${6}
+
+_action_suppression_name=${7}
 
 # 2. FUNCTIONS
 function func_check_http_response(){
     local http_message_body="$1"
     local string_success_response_contains="$2"
+    local filePath="$3"
+    local fileName="$4"
     if [[ "$http_message_body" =~ "$string_success_response_contains" ]]; then # contains
-            echo "Success..."
+        cp -rf "$filePath" "./api_actions/uploaded/${fileName}.${dt}"
+        echo "Success..."
     else
         echo "${dt} ERROR "{$http_message_body}"" >> error.log
         echo "ERROR $http_message_body"
@@ -24,10 +29,21 @@ function func_check_http_response(){
     fi
 }
 
+function func_check_http_status() {
+    local http_code=$1
+    local message_on_failure=$2
+    #echo "HTTP status code: $http_code"
+    if [[ $http_code -lt 200 ]] || [[ $http_code -gt 299 ]]; then
+        echo "${dt} ERROR "{$http_code: $message_on_failure}"" >> error.log
+        echo "ERROR $http_code: $message_on_failure"
+        exit 1
+    fi
+}
+
 # 3. PREPARE REQUEST
 dt=$(date '+%Y-%m-%d_%H-%M-%S')
 
-_payload_path="./api_actions/uploaded/action-suppression-payload-${dt}.json"
+_payload_path="./api_actions/actions/action-suppression-payload-${dt}.json"
 _template_path="./api_actions/action-suppression-payload-template.json"
 
 _header="Content-Type: application/json; charset=utf8"
@@ -49,7 +65,7 @@ fi
 sed -e "s/_action_suppression_name/${_action_suppression_name}/g" -e "s/_action_suppression_start/${_action_suppression_start}/g" -e "s/_action_suppression_end/${_action_suppression_end}/g" "${_template_path}" > "${_payload_path}"
 
 # application id
-allApplications=$(curl -s --user ${_user_credentials} ${_controller_url}/rest/applications?output=JSON)
+allApplications=$(curl -s --user ${_user_credentials} ${_controller_url}/rest/applications?output=JSON ${_proxy_details})
 
 applicationObject=$(jq --arg appName "$_application_name" '.[] | select(.name == $appName)' <<<$allApplications)
 
@@ -60,13 +76,15 @@ fi
 application_id=$(jq '.id' <<< $applicationObject)
 _resource_url="alerting/rest/v1/applications/${application_id}/action-suppressions"
 
-
 # 4. SEND A CREATE REQUEST
-echo "Uploading application supression action"
-response=$(curl -s -X POST --user $_user_credentials $_controller_url/$_resource_url -H "${_header}" --data "@${_payload_path}" )
+echo "Uploading '"${_action_suppression_name}"' application supression action..."
+response=$(curl -s -X POST --user $_user_credentials $_controller_url/$_resource_url -H "${_header}" --data "@${_payload_path}" ${_proxy_details} )
 
 # 5. CHECK RESULT
 expected_response='"id":' # returns id on success
-func_check_http_response "\{$response}" $expected_response
+func_check_http_response "\{$response}" "${expected_response}" "${_payload_path}" "${_action_suppression_name}.json"
+
+#fileName="$(basename -- $_payload_path)"
+#cp -rf "$_payload_path" "./api_actions/uploaded/${fileName}.${dt}"
  
 #echo "response is: $response"
