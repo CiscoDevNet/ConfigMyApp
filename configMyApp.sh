1ce50889086d4cd6b833f6c259acac0c799b0a1d
alex_jov <aleksjovovic@gmail.com>
2020-10-26 10:43:39 +0000

    Remove health rule function from configMyApp; Update readme with argbash;

--- configMyApp.sh
+++ configMyApp.sh
@@ -192,33 +192,6 @@ function func_cleanup() {
     rm -rf $tempFolder
 }
 
-# function func_import_health_rules(){
-#     local appId=$1
-#     local folderPath=$2
-
-#      # get all current health rules for application
-#     allHealthRules=$(curl -s --user ${_user_credentials} ${_controller_url}/alerting/rest/v1/applications/${appId}/health-rules ${_proxy_details})
-
-#     for f in $folderPath; do 
-#         echo "Processing $f health rule template"
-#         # get health rule name from json file
-#         healthRuleName=$(jq -r  '.name' <$f)
-#         # use it to get health rule id (if exists)
-#         healthRuleId=$(jq --arg hrName "$healthRuleName" '.[] | select(.name == $hrName) | .id' <<<$allHealthRules)
-
-#         # create new if health rule id does not exist
-#         if [ "${healthRuleId}" == "" ]; then
-#             httpCode=$(curl -s -o /dev/null -w "%{http_code}" -X POST --user ${_user_credentials} ${_controller_url}/alerting/rest/v1/applications/${appId}/health-rules --header "Content-Type: application/json" --data "@${f}" ${_proxy_details})
-#             func_check_http_status $httpCode "Error occured while importing server health rules."
-#         # overwrite existing health rule only if flag is true
-#         elif [ "${_health_rules_overwrite}" = true ]; then
-#             httpCode=$(curl -s -o /dev/null -w "%{http_code}" -X PUT --user ${_user_credentials} ${_controller_url}/alerting/rest/v1/applications/${appId}/health-rules/${healthRuleId} --header "Content-Type: application/json" --data "@${f}" ${_proxy_details})
-#             func_check_http_status $httpCode "Error occured while importing server health rules."
-#         fi
-
-#     done
-# }
-
 ### END FUNCTIONS ###
 
 url=${_controller_url}${endpoint}
@@ -256,22 +229,7 @@ else
     # proceed as normal
 
     # Health rules
-    source ./modules/health_rules/upload_health_rules.sh "$_controller_url" "$_user_credentials" "$_application_name" "$_proxy_details" "false" "$_health_rules_overwrite" "$_include_sim"
-
-    # # Server Visibility health rules
-    # if [ "${_include_sim}" = true ]; then
-    #     echo "Creating Server Visibility Health Rules...Please wait"
-    #     echo ""
-
-    #     func_import_health_rules $appId "${serverVizHealthRuleFile}"
-    # fi
-
-    # # Application health rules
-    # echo ""
-    # echo "Creating ${_application_name} Health Rules..."
-    # sleep 1
-
-    # func_import_health_rules $appId "${applicationHealthRule}"
+    source ./modules/health_rules/upload_health_rules.sh "$_controller_url" "$_user_credentials" "$_application_name" "$_proxy_details" "$_health_rules_overwrite" "$_include_sim"
 
     echo ""
     sleep 1
@@ -279,7 +237,6 @@ else
 
     echo ""
     echo "Processing Dashboard Template."
-    sleep 1
     echo ""
 
     # Dashboard
