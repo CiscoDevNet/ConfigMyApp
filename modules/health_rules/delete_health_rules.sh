4ee34a5cec6d1f3bc9a3034cae42d514d18743bc
alex_jov <aleksjovovic@gmail.com>
2020-10-30 13:14:25 +0000

    Delete HR from comma separated list;

--- modules/health_rules/delete_health_rules.sh
+++ modules/health_rules/delete_health_rules.sh
@@ -29,23 +29,22 @@ function func_delete_health_rules(){
      # get all current health rules for application
     allHealthRules=$(curl -s --user ${_user_credentials} ${_controller_url}/alerting/rest/v1/applications/${appId}/health-rules ${_proxy_details})
 
-    hrName=${healthRulesToDelete}
-    #for hrName in "${healthRulesToDelete[@]}"; do 
+    echo $healthRulesToDelete | sed -n 1'p' | tr ',' '\n' | while read hrName; do
+        echo "Deleting '$hrName' health rule..."
 
-    echo "Deleting '$hrName' health rule..."
+        # get health rule id (if exists)
+        healthRuleId=$(jq --arg hrName "$hrName" '.[] | select(.name == $hrName) | .id' <<<$allHealthRules)
 
-    # get health rule id (if exists)
-    healthRuleId=$(jq --arg hrName "$hrName" '.[] | select(.name == $hrName) | .id' <<<$allHealthRules)
-
-    # create new if health rule id does not exist
-    if [ ! -z "${healthRuleId// }" ]; then
-        httpCode=$(curl -s -o /dev/null -w "%{http_code}" -X DELETE --user ${_user_credentials} ${_controller_url}/alerting/rest/v1/applications/${appId}/health-rules/${healthRuleId} ${_proxy_details})
-        func_check_http_status $httpCode "Error occured while deleting health rule '${hrName}'."
-        echo "done"
-    else 
-        echo "Health rule '$hrName' not found. No action performed."
-    fi
+        # delete if health rule exists
+        if [ ! -z "${healthRuleId// }" ]; then
+            httpCode=$(curl -s -o /dev/null -w "%{http_code}" -X DELETE --user ${_user_credentials} ${_controller_url}/alerting/rest/v1/applications/${appId}/health-rules/${healthRuleId} ${_proxy_details})
+            func_check_http_status $httpCode "Error occured while deleting health rule '${hrName}'."
+            echo "done"
+        else 
+            echo "Health rule '$hrName' not found. No action performed."
+        fi
         
+    done
 }
 
 # 3. PREPARE 
