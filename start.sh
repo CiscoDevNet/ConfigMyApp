15adf368031d24134657f416c7c3621511bc6e38
alex_jov <aleksjovovic@gmail.com>
2020-11-24 14:47:27 +0000

    Update false flag name;

--- start.sh
+++ start.sh
@@ -322,7 +322,7 @@ parse_commandline()
 				_arg_health_rules_only_explicitly_set=true
 				test "${1:0:5}" = "--no-" && _arg_health_rules_only=false
 				;;
-			--overwrite-no-health-rules|--overwrite-health-rules)
+			--no-overwrite-health-rules|--overwrite-health-rules)
 				_arg_health_rules_overwrite=true
 				_arg_health_rules_overwrite_explicitly_set=true
 				test "${1:0:5}" = "--no-" && _arg_health_rules_overwrite=false
