f295b5d38feb1cf273ac91e867dd98f87379a44a
alex_jov <aleksjovovic@gmail.com>
2021-03-15 17:21:54 +0100

    RBAC get cookie silently;

--- modules/rbac/restui_auth.sh
+++ modules/rbac/restui_auth.sh
@@ -5,7 +5,7 @@ function func_restui_get_cookie() {
     local _user_credentials=${2} # ${username}:${password}
     local _proxy_details=${3} 
 
-    response=$(curl -i -v -s -c cookie.appd --user ${_user_credentials} -X GET ${_controller_url}/auth?action=login ${_proxy_details})
+    response=$(curl -i -s -c cookie.appd --user ${_user_credentials} -X GET ${_controller_url}/auth?action=login ${_proxy_details})
     X_CSRF_TOKEN="$(grep X-CSRF-TOKEN cookie.appd|rev|cut -d$'\t' -f1|rev)"
     X_CSRF_TOKEN_HEADER="`if [ -n "$X_CSRF_TOKEN" ]; then echo "X-CSRF-TOKEN:$X_CSRF_TOKEN"; else echo ''; fi`"
 
